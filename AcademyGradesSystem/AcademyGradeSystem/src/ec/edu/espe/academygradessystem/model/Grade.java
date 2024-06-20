63a8dce9884cd7246e0cd0facc5037d8a982daf3
Zaith Alejandro Manangon Vinueza <a.manangon09@gmail.com>
2024-06-20 08:25:49 -0500

    update

--- AcademyGradesSystem/AcademyGradeSystem/src/ec/edu/espe/academygradessystem/model/Grade.java
+++ AcademyGradesSystem/AcademyGradeSystem/src/ec/edu/espe/academygradessystem/model/Grade.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
@@ -60,66 +59,3 @@ public class Grade {
     
 }
 
-=======
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package ec.edu.espe.academygradessystem.model;
-
-/**
- *
- * @author JEZHE Software Sssosiation Engineers - DCCO ESPE
- */
-import com.google.gson.annotations.Expose;
-
-public class Grade {
-    @Expose
-    private String studentId;
-    @Expose
-    private String subjectCode;
-    @Expose
-    private double grade;
-
-    public Grade(String studentId, String subjectCode, double grade) {
-        this.studentId = studentId;
-        this.subjectCode = subjectCode;
-        this.grade = grade;
-    }
-
-    // Getters and setters
-
-    public String getStudentId() {
-        return studentId;
-    }
-
-    public void setStudentId(String studentId) {
-        this.studentId = studentId;
-    }
-
-    public String getSubjectCode() {
-        return subjectCode;
-    }
-
-    public void setSubjectCode(String subjectCode) {
-        this.subjectCode = subjectCode;
-    }
-
-    public double getGrade() {
-        return grade;
-    }
-
-    public void setGrade(double grade) {
-        this.grade = grade;
-    }
-
-    @Override
-    public String toString() {
-        return "Grade{" + "studentId=" + studentId + ", subjectCode=" + subjectCode + ", grade=" + grade + '}';
-    }
-    
-    
-    
-}
-
->>>>>>> 3c4bd1f38f36c8722408e2412688941930e19604
--- AcademyGradesSystem/AcademyGradeSystem/src/ec/edu/espe/academygradessystem/model/Subject.java
+++ AcademyGradesSystem/AcademyGradeSystem/src/ec/edu/espe/academygradessystem/model/Subject.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
@@ -59,65 +58,3 @@ public class Subject {
     
 }
 
-=======
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package ec.edu.espe.academygradessystem.model;
-
-/**
- *
- * @author JEZHE Software Sssosiation Engineers - DCCO ESPE
- */
-import com.google.gson.annotations.Expose;
-
-public class Subject {
-    @Expose
-    private String code;
-    @Expose
-    private String name;
-    @Expose
-    private int credits;
-
-    public Subject(String code, String name, int credits) {
-        this.code = code;
-        this.name = name;
-        this.credits = credits;
-    }
-
-    // Getters and setters
-
-    public String getCode() {
-        return code;
-    }
-
-    public void setCode(String code) {
-        this.code = code;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getCredits() {
-        return credits;
-    }
-
-    public void setCredits(int credits) {
-        this.credits = credits;
-    }
-
-    @Override
-    public String toString() {
-        return "Subject{" + "code=" + code + ", name=" + name + ", credits=" + credits + '}';
-    }
-    
-    
-}
-
->>>>>>> 3c4bd1f38f36c8722408e2412688941930e19604
