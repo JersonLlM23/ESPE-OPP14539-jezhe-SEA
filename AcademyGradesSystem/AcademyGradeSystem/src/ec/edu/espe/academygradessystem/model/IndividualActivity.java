c0298dbacf75e5fbefaab2da5922b8396fb8d97c
Jerson Stiven Llumiquinga Moreno <jersonllumiquingam23@gmail.com>
2024-06-22 00:43:25 -0500

    Add files via upload

---
+++ AcademyGradesSystem/AcademyGradeSystem/src/ec/edu/espe/academygradessystem/model/IndividualActivity.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ec.edu.espe.academygradessystem.model;
+
+/**
+ *
+ * @author JEZHE Software Sssosiation Engineers - DCCO ESPE
+ */
+public class IndividualActivity {
+    private double grade;
+
+    public IndividualActivity(double grade) {
+        this.grade = grade;
+    }
+
+    public double getGrade() {
+        return grade;
+    }
+
+    public void setGrade(double grade) {
+        this.grade = grade;
+    }
+}
